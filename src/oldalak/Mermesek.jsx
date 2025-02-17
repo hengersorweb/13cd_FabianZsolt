a4ad489a6a6a3eb2c214464a32778175953c8b2c
Fábián Zsolt <fabzso888@hengersor.hu>
2025-02-17 11:30:29 +0100

    Szükséges modulok telepítése

---
+++ src/oldalak/Mermesek.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mermesek = (() => {
+
+    return(
+        <div>
+            <h1>Az olimpia magyar érmesei</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium veniam a temporibus delectus, eveniet unde numquam accusamus repudiandae mollitia dolorem consequuntur ab itaque quasi reprehenderit obcaecati, officia sequi et sed dolorum illo autem id. Reprehenderit assumenda doloremque dolorum itaque cumque debitis, voluptatum officia provident inventore corporis tenetur, illum repudiandae accusamus ipsum. Iure, possimus? Eum aliquid maxime laboriosam voluptatum maiores ipsum cum quam provident fugit veritatis molestiae, labore neque laudantium qui voluptate facere amet corrupti porro ducimus architecto. Cupiditate quidem nisi praesentium, itaque ad recusandae iste sint eum architecto nulla autem repellat earum explicabo labore? Tempore repellendus fugiat soluta laudantium eos.</p>
+        </div>
+    )
+}
+)
+export default Mermesek
