a0688f4653ded4ba93e484b9fa6d53a6fe8e1491
Fábián Zsolt <fabzso888@hengersor.hu>
2025-02-17 12:02:02 +0100

    navbar elkészítése

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <>
       <div>
         <h1>Covid</h1>
+        <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/ermesek" element={<Ermesek />}/>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {Link, NavLink} from "react-router-dom";
 
 const Navbar = (() => {
 
     return(
         <div>
-            <h1>Menu</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium veniam a temporibus delectus, eveniet unde numquam accusamus repudiandae mollitia dolorem consequuntur ab itaque quasi reprehenderit obcaecati, officia sequi et sed dolorum illo autem id. Reprehenderit assumenda doloremque dolorum itaque cumque debitis, voluptatum officia provident inventore corporis tenetur, illum repudiandae accusamus ipsum. Iure, possimus? Eum aliquid maxime laboriosam voluptatum maiores ipsum cum quam provident fugit veritatis molestiae, labore neque laudantium qui voluptate facere amet corrupti porro ducimus architecto. Cupiditate quidem nisi praesentium, itaque ad recusandae iste sint eum architecto nulla autem repellat earum explicabo labore? Tempore repellendus fugiat soluta laudantium eos.</p>
+            <nav>
+                <Link to='/'>Főoldal</Link>
+                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink to='/mermesek'>Az magyar érmesek</NavLink>
+            </nav>
+
+
         </div>
     )
 }
